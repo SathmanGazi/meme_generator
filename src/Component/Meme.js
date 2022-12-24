94f41dda536012b4a9ba28a8f024aa9e1018aff6
Musa Al Sathman Gazi <gazi@usf.edu>
2022-12-24 18:58:46 -0500

    +memeData.js, +useState ->meme.js, +Random Image

--- src/Component/Meme.js
+++ src/Component/Meme.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import '../style.css';
+import memesData from './memesData';
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState('');
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
   return (
-    <div>
-      <main>
+    <main>
+      <div className='form'>
         {/* input parts */}
-        <form className='form'>
-          <input type='text' placeholder='Top Text' className='form--input' />
-          <input
-            type='text'
-            placeholder='Bottom Text'
-            className='form--input'
-          />
-          <button className='form--button'>Get a new meme image</button>
-        </form>
-      </main>
-    </div>
+
+        <input type='text' placeholder='Top Text' className='form--input' />
+        <input type='text' placeholder='Bottom Text' className='form--input' />
+        <button className='form--button' onClick={getMemeImage}>
+          Get a new meme image
+        </button>
+      </div>
+      <img src={memeImage} className='meme--image' />
+    </main>
   );
 }
